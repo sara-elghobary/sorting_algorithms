f7a9ca2219143a9b103e3bbf0e23eda3f1eb9c83
sara-elghobary <soorfour@gmail.com>
2024-01-19 18:46:15 +0200

    h

--- sort.h
+++ sort.h
@@ -20,24 +20,12 @@ typedef struct listint_s
 } listint_t;
 
 
-/* print.c */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void check_list(const listint_t *list);
-void check_array(const int *array, size_t size);
-int *rand_array(int len, int max);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void merge_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 #endif
